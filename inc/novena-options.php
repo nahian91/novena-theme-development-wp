ef81ac766417b7efbf85a123cf8cc0c66223ec97
nahian28 <nahian.instructory@gmail.com>
2022-09-15 04:59:42 +0600

    Updates

--- inc/novena-options.php
+++ inc/novena-options.php
@@ -12,12 +12,14 @@ if(class_exists('CSF')) {
 
     CSF::createSection($prefix, array(
         'id' => 'header_section',
-        'title' => __('Header Section', 'novena')
+        'title' => __('Header Section', 'novena'),
+        'icon'   => 'fas fa-rocket'
     ));
 
     CSF::createSection($prefix, array(
         'parent' => 'header_section',
         'title' => __('Header Info', 'novena'),
+        'icon'   => 'fas fa-rocket',
         'fields' => array(
             array(
                 'id' => 'header_email',
@@ -40,7 +42,145 @@ if(class_exists('CSF')) {
                 'title' => __('Header Right Number', 'novena'),
                 'type' => 'number'
             )
-        )
+        )        
     ));
+
+    CSF::createSection($prefix, array(
+      'parent' => 'header_section',
+      'title' => __('Header Style', 'novena'),
+      'icon'   => 'fas fa-rocket',
+      'fields' => array(
+        array(
+          'id' => 'header_background',
+          'title' => __('Header Background', 'novena'),
+          'type' => 'background',
+          'output' => '.header-top-bar',
+          'default' => array(
+            'background-color' => '#223a66'
+          )
+        ),
+        array(
+            'id' => 'header_color',
+            'title' => __('Header Color', 'novena'),
+            'type' => 'color',
+            'output' => '.top-bar-info li a, .list-inline-item, .top-right-bar a span',
+            'default' => '#fff'
+        )
+      )
+    ));
+
+    CSF::createSection($prefix, array(
+      'id' => 'bannar_section',
+      'title' => __('Bannar Section', 'novena'),
+      'icon'   => 'fas fa-rocket'
+    ));
+
+    CSF::createSection($prefix, array(
+      'parent' => 'bannar_section',
+      'title' => __('Bannar Info','novena'),
+      'icon'   => 'fas fa-rocket',
+      'fields' => array(
+        array(
+          'id' => 'bannar_subtitle',
+          'title' => __('Bannar Subtitle', 'novena'),
+          'type' => 'text'
+        ),
+        array(
+          'id' => 'bannar_title',
+          'title' => __('Bannar Title', 'novena'),
+          'type' => 'text'
+        ),
+        array(
+          'id' => 'bannar_desc',
+          'title' => __('Bannar Description', 'novena'),
+          'type' => 'textarea'
+        ),
+        array(
+          'id' => 'bannar_btn',
+          'title' => __('Bannar Button', 'novena'),
+          'type' => 'link',
+          'add_title' => __('Add Button', 'novena')
+        ),
+        array(
+          'id' => 'bannar_back',
+          'title' => __('Bannar Background', 'novena'),
+          'type' => 'background',
+          'background_color' => false,
+          'output' => '.banner'
+        )
+      )
+    ));
+
+    CSF::createSection($prefix, array(
+      'parent' => 'bannar_section',
+      'title' => __('Bannar Style', 'novena'),
+      'icon'   => 'fas fa-rocket',
+      'fields' => array(
+        array(
+          'id' => 'bannar_separator',
+          'title' => __('Bannar Separator', 'novena'),
+          'type' => 'background',
+          'default' => array(
+            'background-color' => '#e12454'
+          ),
+          'output' => '.divider',
+          'background_image' => false,
+          'background_position' => false,
+          'background_repeat' => false,
+          'background_size' => false,
+          'background_attachment' => false,
+        ),
+        array(
+          'id' => 'bannar_substitle_color',
+          'title' => __('Bannar Subtitle', 'novena'),
+          'type' => 'color',
+          'output' => '.letter-spacing',
+          'default' => '#6F8BA4'
+        ),
+        array(
+          'id' => 'bannar_title_color',
+          'title' => __('Bannar Title', 'novena'),
+          'type' => 'color',
+          'output' => '.banner .block h1',
+          'default' => '#223a66'
+        ),
+        array(
+          'id' => 'bannar_desc_color',
+          'title' => __('Bannar Description', 'novena'),
+          'type' => 'color',
+          'output' => '.banner .block p',
+          'default' => '#6F8BA4'
+        ),
+        array(
+          'id' => 'button_background',
+          'title' => __('Button Background', 'novena'),
+          'type' => 'background',
+          'default' => array(
+            'background-color' => '#e12454'
+          ),
+          'output' => '.btn-main-2',
+          'background_image' => false,
+          'background_position' => false,
+          'background_repeat' => false,
+          'background_size' => false,
+          'background_attachment' => false,
+        ),
+        array(
+          'id' => 'button_background_hover',
+          'title' => __('Button Background Hover', 'novena'),
+          'type' => 'background',
+          'default' => array(
+            'background-color' => '#223a66'
+          ),
+          'output' => '.btn-main-2:hover',
+          'background_image' => false,
+          'background_position' => false,
+          'background_repeat' => false,
+          'background_size' => false,
+          'background_attachment' => false,
+        ),
+      )
+    ));
+    
 
 }
--- template-home.php
+++ template-home.php
@@ -6,7 +6,7 @@ Template Name: Home Template
 
 get_header();?>
 
-
+<?php $options = get_option( 'novena_options' ); ?>
 <!-- Slider Start -->
 <section class="banner">
 	<div class="container">
@@ -14,12 +14,11 @@ get_header();?>
 			<div class="col-lg-6 col-md-12 col-xl-7">
 				<div class="block">
 					<div class="divider mb-3"></div>
-					<span class="text-uppercase text-sm letter-spacing ">Total Health care solution</span>
-					<h1 class="mb-3 mt-3">Your most trusted health partner</h1>
-					
-					<p class="mb-4 pr-5">A repudiandae ipsam labore ipsa voluptatum quidem quae laudantium quisquam aperiam maiores sunt fugit, deserunt rem suscipit placeat.</p>
+					<span class="text-uppercase text-sm letter-spacing "><?php echo $options['bannar_subtitle'];?></span>
+					<h1 class="mb-3 mt-3"><?php echo $options['bannar_title'];?></h1>
+					<p class="mb-4 pr-5"><?php echo $options['bannar_desc'];?></p>
 					<div class="btn-container ">
-						<a href="appoinment.html" target="_blank" class="btn btn-main-2 btn-icon btn-round-full">Make appoinment <i class="icofont-simple-right ml-2  "></i></a>
+						<a href="<?php echo $options['bannar_btn']['url'];?>" target="_blank" class="btn btn-main-2 btn-icon btn-round-full"><?php echo $options['bannar_btn']['text'];?> <i class="icofont-simple-right ml-2  "></i></a>
 					</div>
 				</div>
 			</div>
